c2069efcef85f5e5d9e48e5aae2202c33b372ecc
Ubuntu <azureuser@dappletTemp.xhtobzropgwu3b4uquo01n14gb.cx.internal.cloudapp.net>
2022-04-13 04:41:43 +0000

    done

---
+++ app/javascript/custom/main.js
@@ -0,0 +1,41 @@
+import { connect, Contract, keyStores, WalletConnection } from 'near-api-js';
+import getConfig from './config.js';
+
+function initContract(node_env) {
+    const nearConfig = getConfig(node_env || 'development', 'greeter.wabinab.testnet')
+    const near = await connect(Object.assign({ deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } }, nearConfig));
+
+    window.nearConfig = nearConfig
+    window.near = near
+
+    window.walletConnection = new WalletConnection(near)
+
+    window.accountId = window.walletConnection.getAccountId()
+
+    window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
+      // View methods are read only. They don't modify the state, but usually return some value.
+      viewMethods: ['get_greeting'],
+      // Change methods can modify the state. But you don't receive the returned value when called.
+      changeMethods: ['set_greeting'],
+    })
+}
+
+
+function logout() {
+  window.walletConnection.signOut()
+  // reload page
+  window.location.replace(window.location.origin + window.location.pathname)
+}
+
+function login() {
+  // Allow the current app to make calls to the specified contract on the
+  // user's behalf.
+  // This works by creating a new access key for the user's account and storing
+  // the private key in localStorage.
+  window.walletConnection.requestSignIn(nearConfig.contractName)
+}
+
+
+window.initContract = initContract
+window.logout = logout
+window.login = login
